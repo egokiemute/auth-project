b66435874348171c8ca71f0387682e9dabb4abae
egokiemute <ego.okiemute@gmail.com>
2024-11-13 11:08:41 +0100

    chore: fixed some errors 2

--- frontend/src/components/AuthButton.jsx
+++ frontend/src/components/AuthButton.jsx
@@ -1,49 +1,49 @@
 import { motion } from "framer-motion";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { app } from "../firebase";
-import { useNavigate } from "react-router-dom";
-import { useAuthStore } from "../store/authStore";
+// import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+// import { app } from "../firebase";
+// import { useNavigate } from "react-router-dom";
+// import { useAuthStore } from "../store/authStore";
 
 const AuthButton = () => {
-  const auth = getAuth(app);
-  const navigate = useNavigate();
-  const { setUser } = useAuthStore();
+  // const auth = getAuth(app);
+  // const navigate = useNavigate();
+  // const { setUser } = useAuthStore();
 
-  const handleGoogleLogin = async (e) => {
-    e.preventDefault();
-    const provider = new GoogleAuthProvider();
+  // const handleGoogleLogin = async (e) => {
+  //   e.preventDefault();
+  //   const provider = new GoogleAuthProvider();
 
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
+  //   try {
+  //     const result = await signInWithPopup(auth, provider);
+  //     const user = result.user;
 
-      const userDetails = {
-        name: user.displayName,
-        email: user.email,
-        profilePicture: user.photoURL,
-        isVerified: user.emailVerified,
-      };
+  //     const userDetails = {
+  //       name: user.displayName,
+  //       email: user.email,
+  //       profilePicture: user.photoURL,
+  //       isVerified: user.emailVerified,
+  //     };
 
-      const res = await fetch("http://localhost:8000/api/auth/google", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userDetails),
-      });
+  //     const res = await fetch("http://localhost:8000/api/auth/google", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(userDetails),
+  //     });
 
-      const data = await res.json();
-      if (data.success) {
-        // Set user in the store
-        setUser(data.user); 
-        navigate("/space"); // Navigate directly here
-      } else {
-        console.error("Backend authentication failed:", data.message);
-      }
-    } catch (error) {
-      console.error("Google sign-in error:", error);
-    }
-  };
+  //     const data = await res.json();
+  //     if (data.success) {
+  //       // Set user in the store
+  //       setUser(data.user);
+  //       navigate("/space"); // Navigate directly here
+  //     } else {
+  //       console.error("Backend authentication failed:", data.message);
+  //     }
+  //   } catch (error) {
+  //     console.error("Google sign-in error:", error);
+  //   }
+  // };
 
   return (
     <div className="mt-5 w-full">
@@ -54,7 +54,7 @@ const AuthButton = () => {
                     focus:ring-offset-gray-900 transition duration-200"
         whileHover={{ scale: 1.02 }}
         whileTap={{ scale: 0.98 }}
-        onClick={handleGoogleLogin}
+        // onClick={handleGoogleLogin}
       >
         <img src="/google.svg" alt="google signup" />
         <span>Continue with Google</span>
